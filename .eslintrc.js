0af355466918352c69fb173e181e195e62dd85f8
= <brayanmotavargas@gmail.com>
2017-10-31 10:57:44 -0400

    ESLint rule linebreak-style changing depending on Windows or Unix

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // don't require .vue extension when importing
+    "linebreak-style": ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"],
     'import/extensions': ['error', 'always', {
       'js': 'never',
       'vue': 'never'
